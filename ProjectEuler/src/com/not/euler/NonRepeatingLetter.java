857ed5ef7e2bf3d4553d519aa3179eb9e49590d7
Taran Pawa <tarandeep.singh2007@gmail.com>
2016-01-07 16:55:29 -0500

    adding the solution for first and last non-repeated character

--- ProjectEuler/src/com/not/euler/NonRepeatingLetter.java
+++ ProjectEuler/src/com/not/euler/NonRepeatingLetter.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * Find The First Non Repeated Character In A String. If the word "stress" is
+ * Find The First and last Non Repeated Character In A String. If the word "stress" is
  * input then it should print 't' as output. If the word "teeter" is input then
  * it should print 'r' as output .
  * 
@@ -14,17 +14,37 @@ import java.util.Set;
 public class NonRepeatingLetter {
 
 	public static void main(String args[]) {
-		String input ="teeter";
+		String input = "stress";
+		String answer1 = last(input);
+		System.out.println(answer1);
+
+		char answer2 = first(input);
+		System.out.println(answer2);
+	}
+
+	private static String last(String input) {
 		Set<String> a = new HashSet<String>();
 		String answer = "";
-		for(int i=0;i<input.length();i++){
-			String current=input.substring(i,i+1);
-			if(!a.contains(current)){
-				answer=current;
+		for (int i = 0; i < input.length(); i++) {
+			String current = input.substring(i, i + 1);
+			if (!a.contains(current)) {
+				answer = current;
 				a.add(current);
 			}
 		}
-		System.out.println(answer);
+		return answer;
 	}
-	
+
+	private static char first(String input) {
+		char answer = 0;
+		for (int i = 0; i < input.length(); i++) {
+			char ch = input.charAt(i);
+			if(input.indexOf(ch)==input.lastIndexOf(ch)){
+				answer= ch;
+				break;
+			}
+		}
+		return answer;
+	}
+
 }
